79c523b4ce33e2dc0f857c31c116c724fb83d12b
jprid <johnpridmore.lit@gmail.com>
2017-11-04 12:29:06 -0500

    fixed errors and updated naming conventions

--- AutoVend.sol
+++ AutoVend.sol
@@ -1,35 +1,38 @@
 pragma solidity ^0.4.17;
+import "StringUtils";
+
 /* AutoVend
  * contract for physical vending machine 
  * _creator   = contract owner
  * _inventory = inventory
  */
 contract AutoVend {
-    address _creator;
-    address _coinStorage;
-    bool _testMode;
-    bool    _running;
-    uint8   _stgIdx;
-    uint8   STORAGE_MAX = 4;
+    // automated vending machine contract
+    address  _creator;
+    address  _coinStorage;
+    bool     _testMode;
+    bool     _running;
+    uint32   _stgIdx;
+    uint8    STORAGE_MAX = 6;
     
-    struct item {
+    struct Item {
       string  name;
       uint256 cost;
       uint32  cnt;        
       address supplier;
-      //function resupply() external {}; //NOTE: not possible?
+      function(address) external resupply; //NOTE: not possible?
     }
     
     struct store { //NOTE: This doesn't corrently allow for a store[] to hold items. See inventory note.
-      item items; //NOTE: hella poor naming convention
-      //bool locked=true; //NOTE: not possible.
+      Item item; //NOTE: hella poor naming convention
       bool locked;
+      bool isActive;
     }
     
-    struct inventory { //NOTE: While store is dysfunctional, have adjusted for item[6], instead of store.
-      item[6] strg; //NOTE: storage is a key word!
-      mapping(uint32 => bool) activeItems;
-      mapping(address => item) resupplyAddress;
+    struct inventory { 
+      // NOTE: item[6] instead of stor
+      store[6] strg; //NOTE: storage is a key word!
+      mapping(address => Item) resupplyAddress;
     }
     
     inventory _inv;
@@ -42,76 +45,86 @@ contract AutoVend {
       _creator  = msg.sender; 
       _testMode = testMode; //made bool, should be bool?
       _stgIdx   = 0;
-      //_inv = new inventory(); //hmmmmmmmmm
       // connect the vending machine to le blockchainz
       // this.goLive();
       _running  = true;
     }
     
-    function _addItem(item _item) internal {
-      // TODO: check bounds on _stgIdx (4)
-      // inventory.storage[_stgIdx++] = 
-      if (_stgIdx < 4) {
-        // if we are not at max items
+    function _addItem(Item _item) internal {
+      if (_stgIdx < 4) { // if we are not at max items
         bool success = false;
         for (uint32 i = 0; i < STORAGE_MAX; i++) {
-          if(!_inv.activeItems[i]) {
-            _inv.activeItems[i] = true;
-            _inv.strg[i]     = _item;
-            success              = true;
+          if(!_inv.strg[i].isActive) {
+            _inv.strg[i].isActive  = true;
+            _inv.strg[i].item      = _item;
+            success                = true;
           } 
         }
         // if failed to add to array
         if(!success) {
-          // shit be fucked, u no add
-          // maybe resize array
+          // shit be fucked, u no add:: maybe resize array
           // TODO: !!!! refund user !!!
           revert();
         }
       } 
     }
-    //NOTE: For unlock, lock, and remove we take in a storeIdx as uint8, but pass them items in use. 
-    function _unlock_store(uint8 storeIdx) internal {
+    // NOTE: For unlock, lock, and remove we take in a storeIdx as uint8,
+    //       but pass them items in use. 
+    function _unlock_store(uint32 storeIdx) internal {
       // make api call to physical device
-      //_inv.strg[storeIdx].locked = false; //currently dysfunctional while store struct is dysfunctional. as is checks if item is locked. 
+      _inv.strg[storeIdx].locked = false;
+      // currently dysfunctional while store struct is dysfunctional. as is checks if item is locked. 
     }
     
-    function _lock_store(uint8 storeIdx) internal {
+    function _lock_store(uint32 storeIdx) internal {
       // make api call to physical device
-      //_inv.strg[storeIdx].locked = true;
+      _inv.strg[storeIdx].locked = true;
     }
     
-    function vend(item _item, uint32 qty) external payable {
+    function vend(Item _item, uint32 qty) external payable {
       // TODO: unlock storage => vend qty item
       // update item state
       if (qty > _item.cnt) {
-        _unlock_store(_item);
+        _unlock_store(this.find(_item));
         // TODO: vend here
         _item.cnt-=qty;
-        _lock_store(_item);
+        _lock_store(this.find(_item));
       }
       // handles removing item if item wont be restocked
       needResupply(_item); 
     }
     
-    function needResupply(item _item) internal {
+    function needResupply(Item _item) internal {
       if (_item.cnt == 0) {
         resupply(_item);
-      } else if (_item.cnt == uint32(-1)) {
+      } else if (_item.cnt == uint8(-1)) {
         this.remove(_item); //sends an item, but remove() takes an storeIdx. 
       }
     }
     
-    function remove(uint32 storeIdx) external { //probably should take item, and find its storeIdx
+    function find(Item _item) external returns (uint32)  {
+        for (uint32 i = 0; i < STORAGE_MAX; i++) {
+            if (StringUtils.equal(_inv.strg[i].item.name, _item.name)) {
+                 return i;
+            }
+        }
+    }
+    
+    function remove(Item _item) external {
+        _remove(this.find(_item));
+    }
+    
+    function _remove(uint32 storeIdx) internal { 
+      //probably should take item, and find its storeIdx
       require(msg.sender == _creator);
       delete _inv.strg[storeIdx];
       _inv.activeItems[storeIdx] = false;
       _stgIdx--;
     }
     
-    function resupply(item _item) internal {
+    function resupply(Item _item) internal {
       // TODO: call external resupply contract for item (at _item.resupply) 
-      //_item.resupply(); //<< Do that  <<
+      _item.resupply(); //<< Do that  <<
     }
     
     function turnOff() external {
@@ -122,11 +135,11 @@ contract AutoVend {
 
 // TODO: move into own file
 //       populate the vending machine
-contract vendMeUpFam {
+/* contract vendMeUpFam {
   function popDefaultVendingMachine() {
     // TODO:
   }
-}
+} */
 // NOTES:
 // stock vending machine
 // connect vending machine to ethereum network
