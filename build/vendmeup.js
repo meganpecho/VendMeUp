0856d85787c6d118268f2a0f705af82667ed80d6
Megan Pecho <mepecho@gmail.com>
2017-11-04 22:34:17 -0500

    UI and a few Web3 files

--- build/vendmeup.js
+++ build/vendmeup.js
@@ -1,4 +1,32 @@
+////var Web3 = require('web3');
+////if (typeof web3 !== 'undefined') {
+////  web3 = new Web3(web3.currentProvider);
+////} else {
+////  // set the provider you want from Web3.providers
+////  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+////}
+//var Web3 = 'web3';
+//require([Web3], function(Web3){
+//   var web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+//	console.log(web3);
+//
+////	var balanceWei = web3.eth.getBalance('0x250F688D86856506DB47E447baE4014Eb6D1C19F');
+////	console.log(balanceWei);
+//	
+////	var balance = web3.fromWei(balanceWei, 'ether');
+//	var number = web3.eth.blockNumber;
+////	console.log(balance);
+//	console.log(number);
+//})
+	
+
 $(document).ready(function () {
+//	var Web3 = require('web3');
+
+//	$('.info').append($(balanceWei));
+//	$('.info').append($(balance));
+//	$('.info').append($(number));
+	
 	$('.square').each(function (e) {
 		$(this).on('click', function () {
 			switchDisplay(this);
