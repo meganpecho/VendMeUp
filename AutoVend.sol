02752dc63c61169b79e726a1bf9dd161ea1268a1
jprid <johnpridmore.lit@gmail.com>
2017-11-15 11:16:31 -0600

    documentation me up fam

--- AutoVend.sol
+++ AutoVend.sol
@@ -2,58 +2,54 @@
  * John Pridmore, Artur Oganezov, Megan Pecho
  * 11/14/17
  * CSC299
- *
- * AutoVend
- * contract for physical vending machine
- * _creator   = contract owner
- * _inventory = inventory
  */
 pragma solidity ^0.4.17;
 contract AutoVend {
-    // automated vending machine contract
-    address _creator;
-    address private _coinStorage;
-    bool    private _testMode;
-    bool    private _running;
-    uint32  _stgIdx;
-    uint8   STORAGE_MAX = 6;
+    // member vars
+    address private _creator;       /* creator of this vending machine */
+    address private _coinStorage;   /* cold wallet for machine */
+    bool    private _testMode;      /* is machine in test mode? */
+    bool    private _running;       /* is machine currently running? */
+    uint32  _stgIdx;                /* index of latest, still active item */
+    uint8   STORAGE_MAX = 6;        /* max number of phys. containers allowed */
 
     struct Item {
-      string  name;
-      uint256 cost;
-      uint32  cnt;
-      address supplier;
-      uint32  idx;
-    }
-    // store represents physical containers for Items
+      string  name;          /* the name of the item */
+      uint256 cost;          /* the cost of ea. item */
+      uint32  cnt;           /* the number of items in the machine  */
+      address supplier;      /* the address of the item's supplier  */
+      uint32  idx;           /* the index in the store[] item is in */
+    } 
+      
     struct store {
-      Item item;
-      bool locked;
-      bool isActive;
+      Item item;      /* the item to be stored                               */
+      bool locked;    /* whether this storage container is locked            */
+      bool isActive;  /* whether this storage container's item can be vended */
     }
-    store[6] private stg;
 
-    /* CTOR */
+    // @NOTE: if you make stg public it fucks all the things up
+    store[6] private stg; /* array of storage structs to contain items */
+
+    /* Default ctor
+     * @arg AutoVend
+     * @arg contract for physical vending machine
+     * @arg bool _testMode  = if machine is being tested
+     */
     function AutoVend(bool testMode) public payable {
         _creator  = msg.sender;
-        _testMode = testMode; //made bool, should be bool?
+        _testMode = testMode; 
         _stgIdx   = 0;
         _running  = true;
-    // test storage contract
-    //   for(; _stgIdx < STORAGE_MAX; _stgIdx++) {
-    //       stg[_stgIdx].item = Item("", 0, 0, 0x000000, _stgIdx);
-    //   }
     }
 
-    /* addItem */
+    /* addItem 
+     * @arg uint32  _idx:      the index in stg (storage) to which item is added
+     * @arg string  _name:     the name of the item to be added
+     * @arg uint256 _cost:     the cost of the item to be added
+     * @arg uint32  _cnt:      the number of items
+     * @arg address _supplier: the supplier of the item
+     */
     function addItem(uint32 _idx, string _name, uint256 _cost, uint32 _cnt, address _supplier) public {
-        while(stg[_idx].isActive && _idx < STORAGE_MAX) {
-            _idx++;
-        }
-        if (_idx >= STORAGE_MAX) {
-            /* @todo REVERT */
-
-        }
         Item memory tmp = Item({name:     _name,
                                 cost:     _cost,
                                 cnt:      _cnt,
@@ -61,23 +57,33 @@ contract AutoVend {
                                 idx:      _idx});
         stg[_idx].item = tmp;
         stg[_idx].isActive = true;
-        /* @todo change so that things are much more readable?*/
         _stgIdx = _idx;
     }
 
     /* get Item
      * @NOTE: does not work currently because can't return array of structs
     function getItem(uint32 itemIdx) external view returns (Item) {
-        return stg[itemIdx].item;
+        return stg[itemIdx];
     } */
 
-    /* change Item */
+    /* change Item 
+     * @arg uint32  _idx:      the index in stg (storage) to which item is added
+     * @arg string  _name:     the name of the item to be added
+     * @arg uint256 _cost:     the cost of the item to be added
+     * @arg uint32  _cnt:      the number of items
+     * @arg address _supplier: the supplier of the item
+     */
     function changeItem(uint32 itemIdx, string _name, uint256 _cost, uint32 _cnt, address _supplier) external {
         require(msg.sender == stg[itemIdx].item.supplier);
-        /* TODO: change item logic */
-        addItem(itemIdx, _name, _cost, _cnt, _supplier);
+        // @note: sanity check to ensure old item's information doesn't bleed over
+        stg[itemIdx].item.name     = "";
+        stg[itemIdx].item.cost     = 0;
+        stg[itemIdx].item.cnt      = 0;
+        stg[itemIdx].item.supplier = 0x0;
+        stg[itemIdx].item.idx      = itemIdx;
+        addItem(itemIdx, _name, _cost, _cnt, _supplier);    
     }
-
+    
     /* removeItem
      * removes item and sets storage space to inactive
      * @arg itemidx   (uint32)  item's index in storage array
@@ -87,27 +93,29 @@ contract AutoVend {
      * @arg _supplier (address) the hex address of the item's supplier
      */
     function removeItem(uint32 itemIdx) external {
-        if (msg.sender != stg[itemIdx].item.supplier && msg.sender != _creator) {
+        if (msg.sender != stg[itemIdx].item.supplier && msg.sender != _creator){
             revert();
         }
-        stg[itemIdx].item.name = ""; stg[itemIdx].item.cost = 0;
-        stg[itemIdx].item.cnt = 0; stg[itemIdx].item.supplier = 0x0;
-        stg[itemIdx].item.idx = itemIdx;
-        stg[itemIdx].isActive = false;
+        stg[itemIdx].item.name     = "";
+        stg[itemIdx].item.cost     = 0;
+        stg[itemIdx].item.cnt      = 0;
+        stg[itemIdx].item.supplier = 0x0;
+        stg[itemIdx].item.idx      = itemIdx;
+        stg[itemIdx].isActive      = false;
     }
 
      /* remove item and replace with new one
-     * @arg itemidx   (uint32)  item's index in storage array
-     * @arg _name     (string)  name of item
-     * @arg _cost     (uint256) cost of item
-     * @arg _cnt      (uint32)  no. items in stock
-     * @arg _supplier (address) the hex address of the item's supplier
+     *  @arg itemidx   (uint32)  item's index in storage array
+     *  @arg _name     (string)  name of item
+     *  @arg _cost     (uint256) cost of item
+     *  @arg _cnt      (uint32)  no. items in stock
+     *  @arg _supplier (address) the hex address of the item's supplier
      */
     function removeItem(uint32  itemIdx,
-                        string  _name,
-                        uint256 _cost,
-                        uint32  _cnt,
-                        address _supplier) external {
+                        string    _name,
+                        uint256   _cost,
+                        uint32    _cnt,
+                        address   _supplier) external {
         if (msg.sender != stg[itemIdx].item.supplier && msg.sender != _creator) {
             revert();
         }
@@ -125,26 +133,44 @@ contract AutoVend {
      */
     function vend(uint32 idx, uint32 qty) external payable returns (bool) {
         require(stg[idx].isActive);
+        uint256 tot_val = uint256(qty) * stg[idx].item.cost;
+        require(msg.value >= tot_val);
+        /* @todo: percentage for contract creator? */
         if (stg[idx].item.cnt >= qty) {
             stg[idx].item.cnt -= qty;
-            // @TODO vend/interact with the storage location?
-        } else { // @TODO need to give refund to the
+            stg[idx].item.supplier.transfer(msg.value);            
+        } else { 
             revert(); // call for resupply?
         }
     }
 
+    /* turnOff
+     * turns off the vending machine so that it can't vend
+     */ 
     function turnOff() external {
       require(msg.sender == _creator);
       _running = false;
+      for(uint32 i = 0; i < STORAGE_MAX; i++) {
+        stg[i].isActive = false;
+      }
+      // @todo? destroy(this);
     }
 
-    // UTILITIES
-    /* quick fix to compare strings */
+    /* ------------------------------UTILITIES------------------------------ */
+    
+    /* stringsEqual
+     * @TODO: find better way?
+     * hashes both strings and returns their thing
+     */
     function stringsEqual(string _a, string _b) internal pure returns (bool) {
       return keccak256(_a) == keccak256(_b);
     }
 
-    /* convert bytes32 to string */
+    /* bytes32Tostring 
+     * convert bytes32 to string 
+     * @TODO: cite stack overflow article?
+     * @arg x 32bit array of bytes to be converted
+     */
     function bytes32ToString(bytes32 x) internal pure returns (string a) {
         bytes memory bytesString = new bytes(32);
         uint charCount = 0;
